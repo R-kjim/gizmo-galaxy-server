47b31dad206a4b8f5ca8a0f40badfaa2bc152a68
Philip <membaphilip1@gmail.com>
2024-11-19 11:57:30 +0300

    authorization and timestamp

--- payments.py
+++ payments.py
@@ -6,3 +6,22 @@ import requests
 
 
 load_dotenv()#parses contents in the .env file and makess it available in that file
+#get the variables as declared
+consumer_key=os.getenv("Consumer_key") 
+consumer_secret_key=os.getenv("Consumer_secret")
+
+def authorization(url):
+    plain_text=f'{consumer_key}:{consumer_secret_key}'
+    bytes_obj=bytes(plain_text,'utf-8')
+    bs4=base64.b64encode(bytes_obj)
+    bs4str=bs4.decode()
+    headers={
+        'Authorization':'Basic ' + bs4str
+    }
+    res=requests.get(url,headers=headers)
+    return res.json().get('access_token')
+
+def generate_timestamp():
+    time=datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+    return time
+
