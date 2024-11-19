59a035830736513a033f494b85f9ce65e3de8274
Philip <membaphilip1@gmail.com>
2024-11-19 11:59:16 +0300

    create-password

--- payments.py
+++ payments.py
@@ -25,3 +25,19 @@ def generate_timestamp():
     time=datetime.datetime.now().strftime('%Y%m%d%H%M%S')
     return time
 
+def create_password(shortcode,passkey,timestamp):
+    plain_text = shortcode+passkey+timestamp
+    bytes_obj = bytes(plain_text, 'utf-8')
+    bs4 = base64.b64encode(bytes_obj)
+    bs4 = bs4.decode()
+    return bs4
+
+def format_phone_number(number):
+    number_str=str(number)
+    if number_str.startswith('07') or number_str.startswith('01'):
+        return int('254' + number_str[1:])
+    
+    elif number_str.startswith('254'):
+        return int(number_str)
+    else:
+        return None
